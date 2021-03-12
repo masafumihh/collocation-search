c77f6ef4f791f212808ca585537db9cc78d1fe8e
masafumihh <77046378+masafumihh@users.noreply.github.com>
2021-03-12 13:00:11 +0900

    update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const page = params.get("p");
 const keywords = params.get("keywords");
 
 if (page == "page1") {
-    $display.src = "" + keywords;
+    $display.src = "https://script.google.com/macros/s/AKfycbzs2b3Tvx1vS2CksM5cN6aoteuarnX7SL7ZP0J6r4BHCM8I5fISxvvhDg2h9P88W1W3/exec?p=page1&keywords=" + keywords;
 }
 console.log(keywords);
